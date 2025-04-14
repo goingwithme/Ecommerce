2c9772b5bc94b2d3537f22fac10b68380137cc4f
jaypal katare <katarejaypal@gmail.com>
2025-04-14 16:54:24 +0530

    catagory all the products

--- src/pages/Woman.jsx
+++ src/pages/Woman.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
-
+import React,{useState,useEffect,useContext} from 'react'
+import { CartContext } from '../navcompo/CartContext';
+import axios from 'axios';
 function Woman() {
+
+    const { addToCart } = useContext(CartContext);
+  
+    const [data, setData] = useState([]);
+  
+    const getData = async () => {
+      try {
+        let productData = await axios.get('https://fakestoreapi.com/products');
+  
+        let mens = productData.data.filter((item) => item.category === "women's clothing");
+        setData(mens);
+        console.log(mens);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+  
+    useEffect(() => {
+      getData();
+    }, []);
+
   return (
-    <div>Woman</div>
+    <div>
+      <div className="inner_section">
+        <div className="content">
+          <h4>All Products</h4>
+          <div className="card" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '16px' }}>
+            {data.map((item, index) => (
+              <div className="card d-flex flex-row mb-3" key={index} style={{ maxWidth: '540px' }}>
+                <div className="card-body">
+                  <img src={item.image} className="img-fluid" alt="..." style={{ width: "150px", objectFit: "cover" }} />
+                  <h5 className="card-title" style={{ minHeight: '40px' }}>{item.title}</h5>
+                  <div className='' style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    {/* <button onClick={() => navigate(`/BuyNow`)} style={{ marginRight: '8px' }}>Buy Now</button> */}
+                    <button onClick={() => addToCart(item)}>Add to cart</button> {/* Use the local addToCart */}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
